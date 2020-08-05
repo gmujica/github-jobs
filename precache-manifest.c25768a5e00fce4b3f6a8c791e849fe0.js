8d72e487a6c1b3981881d36106c35dbcef933be6
gmujica <gregorymujica1993@gmail.com>
2020-08-04 21:52:05 -0500

    gh-pages

---
+++ precache-manifest.c25768a5e00fce4b3f6a8c791e849fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46743fd2d490d91bb42839da72ee613d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3505ca2f69599ad7ab13",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f20658f2025dd5b42c20",
+    "url": "/static/js/2.31664eb7.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.31664eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3505ca2f69599ad7ab13",
+    "url": "/static/js/main.e99b8cf1.chunk.js"
+  },
+  {
+    "revision": "fd5ca2d83b2880fa4599",
+    "url": "/static/js/runtime-main.28dc6147.js"
+  }
+]);
